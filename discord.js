63f3df1e5dba1538afc81744ee682817f920e7cd
kaamilmirza <kaamil.mirza.2002@gmail.com>
2022-04-18 10:04:23 +0530

    Voice channel join working

--- discord.js
+++ discord.js
@@ -2,6 +2,10 @@ const {Client, Intents,MessageEmbed} = require('discord.js');
 const discordVoice  = require('@discordjs/voice');
 const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES]});
 require('dotenv').config();
+const ytdl = require("ytdl-core");
+
+
+
 client.on('ready',()=>{
   console.log(`Logged in as ${client.user.tag}!` );
 });
@@ -24,21 +28,64 @@ client.on('message', async msg => {
 
 
 client.on('message', async msg => {
-  const connection = joinVoiceChannel.joinVoiceChannel({
-    channelId: msg.channel.id,
+  if(msg.author.bot) //checks if the messages are bot's or not
+    return;
+
+  if(!msg.content.startsWith(prefix)) //checks if bot commands or not
+    return;
+
+
+  const connection = discordVoice.joinVoiceChannel({
+    channelId: msg.member.voice.channel.id, //voice channel ID taken from memeber
     guildId: msg.guild.id,
     adapterCreator: msg.guild.voiceAdapterCreator,
-  });
-      
+  });  // to establish connection of bot and voice channel
+  
+  const serverQueue = queue.get(msg.guild.id);
 
-  if(msg.author.bot)
-  return;
-  if (msg.content === prefix + "voice") {
-      player.play(resource);
-      connection.subscribe(player);
-      console.log("Bot sent to voice");
-  };
+  if(msg.content.startsWith(`${prefix}play`)){
+    execute(msg, serverQueue);
+    return;
+  }
+
+  else if(msg.content.startsWith(`${prefix}skip`)){
+    skip(msg,serverQueue);
+    return;
+  }
+
+  else if(msg.content.startsWith(`${prefix}stop`)){
+    stop(msg, serverQueue);
+    return;
+  }
+
+  else{
+    msg.channel.send("You need to enter to enter a valid command!");
+  }
+  // if (msg.content === prefix + "voice") {
+  //     player.play(resource);
+  //     connection.subscribe(player);
+  //     console.log("Bot sent to voice");
+  // }
 });
+
+async function execute(message, serverQueue){
+  const args = message.content.split(" ");
+
+  const voiceChannel = message.member.voice.channel;
+  if(!voiceChannel){
+    return message.channel.send("You need to be in a voice channel!");
+  }
+
+  const permission = voiceChannel.permissionsFor(message.client.user);
+  if(!permission.has("CONNECT") || !permission.has("SPEAK")){
+    return message.channel.send("Need permissions!");
+  }
+}
+
+
+
+
+
 
 
 
